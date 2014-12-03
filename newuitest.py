2a42082fab68190f56b4af81ae0c26106dd04cf9
nikhilsarma <nikhilsarmak@gmail.com>
2014-12-04 01:05:37 +0530

    final v 1.1

--- newuitest.py
+++ newuitest.py
@@ -1,4 +1,4 @@
-from ofc_copy import *
+from ofc_copytest import *
 #from xl_comp_new import *
 from Tkinter import *
 import tkFileDialog, random
@@ -23,39 +23,38 @@ Label(root, text="Compared to : ",bg=BGCOL, relief=RIDGE).grid(row=17)
 e1 = Entry(root)
 e2 = Entry(root)
 e3 = Entry(root)
+e4 = Entry(root)
+
 e1.place(x=220, y=115, width=145)
 e2.place(x=220, y=190, width=145)
 e3.place(x=220, y=220, width=145)
-
+e4.place(x=145, y=320, width=50)
+e4.insert(0, 0)
 rep = []
+
 def to_comp():
     
     col = color.get()
-    print col
+    if res_mode.get():
+        eps = e4.get()
+    else:
+        eps = 0
     fd = e1.get()
     f1 = e2.get()
     f2 = e3.get()
-    try:
-        
-        if f1.endswith('.xlsx') != f2.endswith('.xlsx'):
-            showwarning("Something happend! ","Cannot Compare different files")             
-        if fd == '' and f1 == '' and f2 == '':
-            showerror("Damn! ", "Please select files to compare")
-        elif fd == '' and (f1 == '' or f2 == ''):
-            showwarning("Uh Oh!", "Enter either a folder OR The two files to compare")
-        else:
-            res = the_mess([fd,f1,f2,col])
-            if res:
-                showinfo("Done!","Finished Comparing!")
-            rep.append(res)
-    except Exception as exp:
-        showwarning("Something happend! ",str(exp.__class__.__name__) + ": " +str(exp.message))
+    #print col,eps
+    res = the_mess([fd,f1,f2,col,float(eps)])
+    if res:
+        showinfo("Done!","Finished Comparing!")
+        rep.append(res)
+    
+
 def gen_report():
 
-    print rep
+    #print rep
     
     rtxt = "Exec.time\tWorkBook [Result]\t\tWorkSheets\tResult"
-    u = "-"*(len(rtxt)+32)
+    u = "-"*(len(rtxt)+38)
     print str('\n\n') + u
     print rtxt + str('\n')  + u
     #msg = "Trust because you are willing to accept the risk, not because it's safe or certain"
@@ -64,18 +63,10 @@ def gen_report():
         for e in rep[0]:
             print str(e[0]) + str('\t\t') +  e[1] +str(" Vs ")+e[2]+ " ["+ e[3].upper() + "]"
             rep1(e[4])
-            #print e[3],
-            if e[3] == "Fail":
-                ecnt += 1
-        if ecnt >= 1:
-            #msg = random.choice(freport)
-            print str('\n') + "Oops!" + str('\n') + random.choice(freport)
-        else:
-            print str('\n') + "Cool! but," + str('\n') + msg
         if len(rep) != 0:
             del rep[0]
     except Exception as e:
-        print "No report generated" + str(e)
+        print "No report generated: " + str(e)
     
 def dir_open():
     dir_path = tkFileDialog.askdirectory()
@@ -98,11 +89,13 @@ def to_reset():
     e1.delete(0, END)
     e2.delete(0, END)
     e3.delete(0, END)
+    #e4.delete(0, END)
+    cb.deselect()
 
-#res_mode = IntVar()
-clist = ["Yellow", "Olive", "Aqua","Cocoa"]
+res_mode = IntVar()
+clist = ["Yellow", "Olive", "Aqua","Cocoa","Orange"]
 color = StringVar(root)
-color.set("Yellow")
+color.set("Orange")
 Button(root, text='select',bg=BGCOL, command=dir_open).grid(row=10, column=3, sticky=NW, pady=4)
 Button(root, text='select', bg=BGCOL,command=file_open1).grid(row=15, column=3, sticky=NW, pady=4)
 Button(root, text='select', bg=BGCOL, command=file_open2).grid(row=17, column=3, sticky=NW, pady=4)
@@ -111,16 +104,22 @@ Button(root, text='compare',bg='brown', command=to_comp).grid(row=20,column=1, s
 Button(root, text='reset', command=to_reset).grid(row=20,column=2, sticky=W, pady=4)
 Button(root, text='Gen..report',bg="green", command=gen_report).grid(row=21,column=1, sticky=W, pady=3)
 Button(root, text='About',bg="yellow", command='').grid(row=23,column=5, sticky=W, pady=3)
-#Checkbutton(root, text="write diff. to new file", variable=res_mode).grid(row=23, sticky=W)
+cb = Checkbutton(root, text="ignore differences <=",bg=BGCOL, variable=res_mode).grid(row=23, sticky=W)
+
+#Checkbutton(root, text="ignore differences less than", variable=res_mode,bg=BGCOL).place(x=10, y=30, width=175)
 Label(root, text="Mark Color : ",bg=BGCOL, relief=RIDGE).grid(row=7,column=0)
-OptionMenu(root,color,*clist).grid(row=7,column=1,sticky=W)
-#OptionMenu.configure(bg='green')
-#cw["menu"].config(bg="GREEN")
+#Label(root, text="Mark Color : ",bg=BGCOL, relief=RIDGE).place(x=180, y=30, width=100)
+om = OptionMenu(root,color,*clist)
+om.config(bg = BGCOL)
+om['menu'].config(bg = BGCOL)
+#w.place(x=220, y=30, width=75)
+om.grid(row=7,column=1,sticky=W)
+
 #Report generation
 def rep1(dic):
     for k,v in dic.items():
         print str(k).rjust(60) + str('\t') + str(v[0]) + str(': ')+str(v[1])
-
+    #print str('\n\n') + u.gen_report
 
 def rep3(dic):
 	for k,v in dic.items():
@@ -131,11 +130,6 @@ def rep2(dic):
 	for k,v in dic.items():
 		print str("\t\t\t\t\t") + str(k) + str("\t\t") + str(v[0]) + str("\t") + str(v[1].__class__.__name__) + str(": ") + str(v[1])
 
-freport = ["I'm not afraid of the cemetery. It's the only place the ghosts don't follow me...", 
-"The lights flicker. I put the pillow over my head, so I won't hear the screams this time...",
-"I burned the dolls even though my children cried. They did not understand my fear because they assumed I was who moved the dolls into their beds each night...",
-"She asked why I was breathing so heavily. I wasn't...",
-'''"I can't sleep" she whispered, crawling into bed with me. I woke up cold, clutching the dress she was buried in...''']
 
 
 mainloop()
---
+++ ofc_copytest.py
@@ -0,0 +1,168 @@
+from openpyxl import Workbook, load_workbook
+from openpyxl.comments import Comment
+from openpyxl.styles import Style, fills, PatternFill, Color
+import datetime, time,os
+#import os, xlrd
+
+coldic = {'Cocoa':'D2691E','Aqua':'7EC0EE','Olive':'89892B','Yellow':'CCA300','Orange':'FF8000'} 
+
+def open_xls_as_xlsx(filename):
+    # first open using xlrd
+    ob = xlrd.open_workbook(filename, formatting_info=True)
+    nb = Workbook()
+    nb.remove_sheet(nb.worksheets[0])
+    index = 0
+    nsheets = ob.nsheets
+    
+    while index < ob.nsheets:
+        os = ob.sheet_by_index(index)
+        nrows = os.nrows
+        ncols = os.ncols
+
+        # prepare a xlsx sheet
+        ns = nb.create_sheet(index)
+        ns.title = os.name
+        #print ns.title,os.name
+        for row in xrange(1, nrows+1):
+            for col in xrange(1, ncols+1):
+                nc = ns.cell(row=row, column=col)
+                oc = os.cell(row-1, col-1)
+                nc.value = oc.value
+                old_format = ob.format_map[ob.xf_list[oc.xf_index].format_key].format_str
+                nc.number_format = old_format
+        index += 1    
+        #nb.save("c:\\users\\nikil\\Desktop\\sample1234.xlsx")
+    return nb
+
+def compare(s1, s2, w1n,w2n, col, eps):
+
+    #rowrange = max(s1.get_highest_row(), s2.get_highest_row())
+    #colrange = max(s1.get_highest_column(), s2.get_highest_column())
+    #print rowrange,colrange
+    #eps = 10
+    rowrange = max(s1.max_row, s2.max_row)
+    colrange = max(s1.max_column, s2.max_column)
+    cfill = Style(fill=PatternFill(patternType='solid', fgColor=Color(coldic[col])))
+    succ = "Pass"
+    e = 'Data Matches!'
+    try:
+        for i in xrange(1,rowrange+1):
+            for j in xrange(1,colrange+1):
+                #time.sleep(0.5)
+                x = s1.cell(row=i,column=j)
+                y = s2.cell(row=i,column=j)
+                xfor_code,yfor_code = x.number_format,y.number_format
+                
+                a = x.value
+                b = y.value
+                nt = None
+                dt = datetime.datetime.today()
+                tlist = [type(int()), type(float()),type(long()), type(nt)]
+                numlist = [type(int()), type(float()),type(long())]
+                #xfor_code = s2.cell(row=i,column=j).number_format
+                if (type(a) not in numlist or x.number_format == '@') and (type(b) not in numlist or y.number_format == '@'):
+                    a,b = unicode(a),unicode(b)
+                    #print x.number_format+str('->')+y.number_format
+                    #print a + str('->')+ b
+                    #print str(type(a)) +str('->')+str(type(b))
+                if a != b:
+                    succ = "Fail"
+                    e = "Data Mismatches"
+                    comtxt = None
+                    
+                    if type(a) in numlist and (type(b) == type(nt) or type(b) == type(unicode())):
+                        comtxt = str(w1n)+": " + unicode(a)+ ". diff: "+ unicode(a)
+                    
+                    elif type(b) in numlist and (type(a) == type(nt) or type(a) == type(unicode())):
+                        comtxt = str(w1n)+": " + unicode(a)+ ". diff: "+ unicode(b)
+                    
+                    elif type(a) in numlist and type(b) in numlist:
+                        if abs(a-b) <= eps:
+                            #print "ignored" + str(a) + str(" and ") +str(b)
+                            continue
+                        comtxt = str(w1n)+": " + unicode(a)+ ". diff: "+ unicode(b-a)
+
+                    elif type(a) == type(unicode()) and type(b) == type(unicode()):
+                        comtxt = str(w1n)+": " + unicode(a)+ "."
+                
+                    comment = Comment(comtxt, w2n)
+                    y.style = cfill
+                    y.comment = comment
+                    y.number_format = yfor_code
+                else:
+                    comtxt = None
+    except KeyboardInterrupt:
+        succ = "Abort"
+        e = "KeyboardInterrupt"
+    except Exception as e:
+        succ = "Abort"
+            
+    return [succ,e]
+              
+
+def the_mess(l):
+    col,eps = l[3],l[4]
+    #print l
+    print "Comparision started...!"
+    report = []
+    if l[1] != '' and l[2] != '':
+        f1 = l[1]
+        f2 = l[2]
+        work_dir = l[2][:l[2].rfind('/')].replace('/','//')
+        os.chdir(work_dir)
+        rprt = the_mayhem(f1,f2,col,eps)
+        report.append(rprt)
+    elif l[0] != '':
+        work_dir = l[0].replace("/","//")
+        os.chdir(work_dir)
+        #print "successfully changed path!"
+        files_list = filter(lambda x: x.endswith('.xlsx'), os.listdir(work_dir))
+        #files_list = filter(lambda x: x.endswith('.xlsx') or x.endswith('.xls'), os.listdir(work_dir))
+        files_list.sort()
+        lfile = len(files_list)
+        cnt = 0
+        while cnt < lfile:
+            f1 = files_list[cnt]
+            f2 = files_list[cnt+1]
+            cnt += 2
+            rprt = the_mayhem(f1,f2,col,eps)
+            report.append(rprt)
+    return report
+    #print report
+
+def the_mayhem(f1,f2,col,eps):
+    print "Comparing... " +f1 +" vs " + f2
+    t1 = time.time()
+    if f1.endswith('.xls') and f2.endswith('.xls'):
+        w1 = open_xls_as_xlsx(f1)
+        w2 = open_xls_as_xlsx(f2)
+    else:
+        w1 = load_workbook(f1)
+        w2 = load_workbook(f2)
+    w1n = f1[f1.rfind('/')+1:f1.find('.')]
+    w2n = f2[f2.rfind('/')+1:f2.find('.')]
+    wbsucc = "Pass"
+    sdic = {}
+
+    for i in xrange(len(w1.worksheets)):
+        for j in xrange(i,i+1):
+            smsg = compare(w1.worksheets[j], w2.worksheets[j],w1n,w2n,col,eps)
+            sdic[w1.worksheets[j].title] = smsg[0],smsg[1]
+            if f2.endswith('.xls') and smsg[0] == 'Fail':
+                w2.save(w2n + str('.xlsx'))
+            elif f2.endswith('.xlsx') and smsg[0] == 'Fail':
+                w2.save(f2)
+    for e in sdic.values():
+        if "Pass" not in e[0]:
+            wbsucc = "Fail"
+    
+    t2 = time.time()
+    ttime = round((t2-t1),2)
+    #print sdic
+    return [ttime,w1n,w2n,wbsucc,sdic]
+   
+
+
+if __name__ == '__main__':
+    main()
+
